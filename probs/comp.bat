da48d3205c89c823a527e619f00f515513b321f2
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2020-02-04 11:41:17 +0000

    Delete useless scripts

--- probs/comp.bat
+++
@@ -1,5 +0,0 @@
-set N=11
-pdflatex probs%N%
-pdflatex -job-name=sols%N% --extra-mem-bot=1000000 --extra-mem-top=1000000 \def\SOLS{1} \input{probs%N%}
-copy probs%N%.pdf c:\www\webroot\pm1nps\courses\MAS243\probs
-copy sols%N%.pdf c:\www\webroot\pm1nps\courses\MAS243\probs
--- probs/comp_all.bat
+++
@@ -1,5 +0,0 @@
-for %%n in (01 02 03 04 05 06 07 08 09 10 11) do (
-pdflatex probs%%n
-pdflatex -job-name=sols%%n --extra-mem-bot=1000000 --extra-mem-top=1000000 \def\SOLS{1} \input{probs%%n}
-)
-
