5628799b8c5295b8f41dffc1900db940317b7adf
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2020-02-04 11:15:30 +0000

    Remove useless scripts

--- lectures/do_publish.bat
+++
@@ -1,7 +0,0 @@
-set webdir=C:\www\webroot\pm1nps\courses\MAS243
-
-copy lecture%1.pdf %webdir%\lecture\lecture%1.pdf
-
-SET HOME=D:\Documents and Settings\Neil Strickland\My Documents
-
-sitecopy.exe --update mycpanel
