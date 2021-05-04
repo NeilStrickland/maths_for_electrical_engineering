f8f2cdd184bf10b244fb6d5ad031131baae208c3
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2021-05-04 19:40:34 +0100

    Tidy up

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -2,4 +2,5 @@ cp ../notes/MAS243.pdf ../docs/notes/MAS243.pdf
 cp ../lectures/all_lectures.pdf ../docs/lectures
 cp ../lectures/all_handouts.pdf ../docs/lectures
 cp ../probs/all_probs.pdf ../docs/probs
+cp -r ../demos ../docs
 
